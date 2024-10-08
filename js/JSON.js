54678a36cc97f574426c48f63e9759691798d1d4
khushi-apu <tabassumkhushi17@gmail.com>
2024-10-08 06:01:41 +0600

    JSON parse

--- js/JSON.js
+++ js/JSON.js
@@ -1,4 +1,25 @@
 const user = {id:1, name:'Gorib Amir', job:'actor'}
  const stringified=JSON.stringify(user)
 // console.log(user) 
- console.log(stringified);
+//  console.log(stringified);
+
+ const shop=
+{
+    owner:'Alia',
+    address:
+    {
+        street:'everyone are same',
+        city:'ranbir',
+        countery:'bd'
+    },
+    products: ['laptop', 'mic' , 'monitor'],
+    revenue:45200,
+    isOpen: true,
+    isNew: false
+}
+
+console.log(shop)
+const shopJSON = JSON.stringify(shop)
+console.log(shopJSON)
+const shopObj=JSON.parse(shopJSON)
+console.log(shopObj)
